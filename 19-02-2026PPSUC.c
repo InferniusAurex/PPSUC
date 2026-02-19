533dae24a684df40064fbf890925e76c2684941d
Infernius Aurex <101462689+InferniusAurex@users.noreply.github.com>
2026-02-19 12:46:10 +0530

    Add files via upload

---
+++ 19-02-2026PPSUC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int a=1;
+float b=17.545;
+char c='A';
+
+printf("Name:%c\n",c);
+printf("Age:%d\n",a);
+printf("height:%.2f\n",b);
+printf("total:%f\n",a+b);
+return 0;
+}
